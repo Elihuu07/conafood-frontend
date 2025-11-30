c2b192ce9fbb0012790dddaea51783591b093519
Elihuu07 <elihujorge@gmail.com>
2025-11-29 23:03:56 -0600

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://conafood.onrender.com/api";
+const API_URL = "https://conafood.onrender.com";
 
 async function cargarMenu() {
   const res = await fetch(API_URL + "/menu");
